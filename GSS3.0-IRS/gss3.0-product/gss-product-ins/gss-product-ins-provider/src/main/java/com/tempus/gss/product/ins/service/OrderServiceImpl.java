615c3ce59790e6e29fa3e7f9bed776c26ca9c1e6
liang.ling@tempus.cn <liang.ling@tempus.cn>
2018-03-26 17:37:40 +0800

    修复保险预订bug

--- GSS3.0-IRS/gss3.0-product/gss-product-ins/gss-product-ins-provider/src/main/java/com/tempus/gss/product/ins/service/OrderServiceImpl.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-ins/gss-product-ins-provider/src/main/java/com/tempus/gss/product/ins/service/OrderServiceImpl.java
@@ -481,7 +481,7 @@ public class OrderServiceImpl implements IOrderService {
         }
         Calendar ca = Calendar.getInstance();
         ca.setTime(saleOrderExt.getEffectDate());
-        ca.add(Calendar.DATE, saleOrderExt.getInsurance().getEndDays());
+        ca.add(Calendar.DATE, insurance.getEndDays());
         SaleOrderExt.setExpireDate(ca.getTime());
         //存储总保险分数
         saleOrderExt.setSumCopies(personnum);
