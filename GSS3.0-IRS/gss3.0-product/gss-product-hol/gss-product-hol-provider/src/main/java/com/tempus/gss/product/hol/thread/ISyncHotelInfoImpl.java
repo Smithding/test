a3972e0c0d1430706166a2ea411ee1206735751a
kai.yang <kai.yang@tempus.cn>
2018-05-11 15:04:45 +0800

    修改酒店查询

---
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-provider/src/main/java/com/tempus/gss/product/hol/thread/ISyncHotelInfoImpl.java
@@ -0,0 +1,54 @@
+package com.tempus.gss.product.hol.thread;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.tempus.gss.exception.GSSException;
+import com.tempus.gss.product.hol.api.entity.request.HotelListSearchReq;
+import com.tempus.gss.product.hol.api.entity.response.TCResponse;
+import com.tempus.gss.product.hol.api.entity.response.tc.ResBaseInfo;
+import com.tempus.gss.product.hol.api.service.IBQYHotelSupplierService;
+import com.tempus.gss.product.hol.api.syn.ISyncHotelInfo;
+import com.tempus.gss.product.hol.api.syn.ITCHotelSupplierService;
+import com.tempus.gss.vo.Agent;
+
+public class ISyncHotelInfoImpl implements ISyncHotelInfo {
+	@Autowired
+	private ITCHotelSupplierService iTCHotelSupplierService;
+	
+	@Autowired
+	IBQYHotelSupplierService iBQYHotelSupplierService;
+
+	@Override
+	public TCResponse<ResBaseInfo> queryHotelList(Agent agent, HotelListSearchReq hotelSearchReq) throws GSSException {
+		/*Future<TCResponse<ResBaseInfo>> queryHotelList = iTCHotelSupplierService.queryHotelList(agent, hotelSearchReq);
+		Future<TCResponse<ResBaseInfo>> queryHotelList2 = iBQYHotelSupplierService.queryHotelList(hotelSearchReq);
+		TCResponse<ResBaseInfo> tcResponse = null;
+		TCResponse<ResBaseInfo> bqyResponse = null;
+		try {
+			//Future<TCResponse<ResBaseInfo>> future = RpcContext.getContext().getFuture();
+			
+			while(true) {
+				if(queryHotelList.isDone() && queryHotelList2.isDone()) {
+					tcResponse = queryHotelList.get();
+					bqyResponse = queryHotelList2.get();
+					break;
+				}
+			}
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		return tcResponse;*/
+		return null;
+	}
+	
+}	
