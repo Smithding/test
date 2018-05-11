1542387b0bafda807c7ab22c3b503dd93c1c4203
kai.yang <kai.yang@tempus.cn>
2018-05-11 17:26:53 +0800

    多线程异步调用类

--- GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-provider/src/main/java/com/tempus/gss/product/hol/thread/ISyncHotelInfoImpl.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-provider/src/main/java/com/tempus/gss/product/hol/thread/ISyncHotelInfoImpl.java
@@ -1,11 +1,11 @@
 package com.tempus.gss.product.hol.thread;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.tempus.gss.bbp.util.StringUtil;
 import com.tempus.gss.exception.GSSException;
 import com.tempus.gss.product.hol.api.entity.request.HotelListSearchReq;
 import com.tempus.gss.product.hol.api.entity.response.TCResponse;
@@ -15,6 +15,7 @@ import com.tempus.gss.product.hol.api.syn.ISyncHotelInfo;
 import com.tempus.gss.product.hol.api.syn.ITCHotelSupplierService;
 import com.tempus.gss.vo.Agent;
 
+@Service
 public class ISyncHotelInfoImpl implements ISyncHotelInfo {
 	@Autowired
 	private ITCHotelSupplierService iTCHotelSupplierService;
@@ -24,7 +25,7 @@ public class ISyncHotelInfoImpl implements ISyncHotelInfo {
 
 	@Override
 	public TCResponse<ResBaseInfo> queryHotelList(Agent agent, HotelListSearchReq hotelSearchReq) throws GSSException {
-		/*Future<TCResponse<ResBaseInfo>> queryHotelList = iTCHotelSupplierService.queryHotelList(agent, hotelSearchReq);
+		Future<TCResponse<ResBaseInfo>> queryHotelList = iTCHotelSupplierService.queryHotelList(agent, hotelSearchReq);
 		Future<TCResponse<ResBaseInfo>> queryHotelList2 = iBQYHotelSupplierService.queryHotelList(hotelSearchReq);
 		TCResponse<ResBaseInfo> tcResponse = null;
 		TCResponse<ResBaseInfo> bqyResponse = null;
@@ -35,10 +36,17 @@ public class ISyncHotelInfoImpl implements ISyncHotelInfo {
 				if(queryHotelList.isDone() && queryHotelList2.isDone()) {
 					tcResponse = queryHotelList.get();
 					bqyResponse = queryHotelList2.get();
+					/*System.out.println("bqy Size: "+bqyResponse.getResponseResult().size());
+					System.out.println(JsonUtil.toJson(bqyResponse.getResponseResult()));
+					for(ResBaseInfo rs : bqyResponse.getResponseResult()) {
+						System.out.println("bqy: "+rs.getResName());
+					}*/
 					break;
 				}
 			}
-			
+			if(StringUtil.isNullOrEmpty(hotelSearchReq.getAirRailWay())) {
+				
+			}
 			
 			
 		} catch (Exception e) {
@@ -47,8 +55,8 @@ public class ISyncHotelInfoImpl implements ISyncHotelInfo {
 		
 		
 		
-		return tcResponse;*/
-		return null;
+		return tcResponse;
+		//return null;
 	}
 	
 }	
