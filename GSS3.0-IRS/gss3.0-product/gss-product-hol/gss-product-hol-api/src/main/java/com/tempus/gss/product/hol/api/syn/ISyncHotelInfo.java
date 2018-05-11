68da14446880a15c2c19d142bf352fd1d21f9056
kai.yang <kai.yang@tempus.cn>
2018-05-11 13:49:50 +0800

    酒店异步查询类

---
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/syn/ISyncHotelInfo.java
@@ -0,0 +1,15 @@
+package com.tempus.gss.product.hol.api.syn;
+
+import java.util.concurrent.Future;
+
+import com.tempus.gss.exception.GSSException;
+import com.tempus.gss.product.hol.api.entity.request.HotelListSearchReq;
+import com.tempus.gss.product.hol.api.entity.response.TCResponse;
+import com.tempus.gss.product.hol.api.entity.response.tc.ResBaseInfo;
+import com.tempus.gss.vo.Agent;
+
+public interface ISyncHotelInfo {
+	
+	public TCResponse<ResBaseInfo> queryHotelList(Agent agent, HotelListSearchReq hotelSearchReq) throws GSSException;
+	
+}
