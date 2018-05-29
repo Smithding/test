e139b3919ca24b7eaff8f0f3f880b118766aa71f
kai.yang <kai.yang@tempus.cn>
2018-05-29 11:45:35 +0800

    shortIntro参数忽略

--- GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/entity/response/tc/ResBaseInfo.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/entity/response/tc/ResBaseInfo.java
@@ -465,6 +465,7 @@ public class ResBaseInfo implements Serializable{
 	public String getShortIntro() {
 		return shortIntro;
 	}
+	@JSONField(deserialize=false)
 	public void setShortIntro(String shortIntro) {
 		this.shortIntro = shortIntro;
 	}
