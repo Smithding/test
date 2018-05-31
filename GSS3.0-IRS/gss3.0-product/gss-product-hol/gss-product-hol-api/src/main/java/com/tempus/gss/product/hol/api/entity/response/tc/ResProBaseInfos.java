dca11e4b7cc0888c8a70796d5c68e7ea1dc7a3af
kai.yang <kai.yang@tempus.cn>
2018-05-31 19:35:44 +0800

    增加房间列表类

---
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/entity/response/tc/ResProBaseInfos.java
@@ -0,0 +1,38 @@
+package com.tempus.gss.product.hol.api.entity.response.tc;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+public class ResProBaseInfos implements Serializable{
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 酒店ID
+	 */
+	@JSONField(name = "Id")
+	private Long id;
+	
+	/**
+	 * 酒店房型信息
+	 */
+	@JSONField(name = "ResProBaseInfos")
+	private List<ResProBaseInfo> resProBaseInfos;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<ResProBaseInfo> getResProBaseInfos() {
+		return resProBaseInfos;
+	}
+
+	public void setResProBaseInfos(List<ResProBaseInfo> resProBaseInfos) {
+		this.resProBaseInfos = resProBaseInfos;
+	}
+	
+}
