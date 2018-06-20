9b868bd17390e32620451c81dffcb9711a1203f7
lizhi <zhi.li@tempus.cn>
2018-06-20 09:30:01 +0800

    广州0604测试bug修复 杂费单管理

--- GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-api/src/main/java/com/tempus/gss/product/ift/api/service/IExtraOrderService.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-api/src/main/java/com/tempus/gss/product/ift/api/service/IExtraOrderService.java
@@ -3,13 +3,9 @@ package com.tempus.gss.product.ift.api.service;
 import com.tempus.gss.controller.Result;
 import com.tempus.gss.order.entity.DifferenceOrder;
 import com.tempus.gss.product.common.entity.RequestWithActor;
-import com.tempus.gss.product.ift.api.entity.vo.ExtraOrderQueryVo;
-import com.tempus.gss.vo.Agent;
-
 public interface IExtraOrderService {
     /**
      * 创建杂费单
-     * @param agent
      * @param requestWithActor
      */
     public Result createExtraOrder(RequestWithActor<DifferenceOrder> requestWithActor);
