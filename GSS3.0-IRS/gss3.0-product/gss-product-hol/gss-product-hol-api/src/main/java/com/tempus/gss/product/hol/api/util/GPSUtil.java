116d9327597be0a763017e386d0386bc70e35f04
kai.yang <kai.yang@tempus.cn>
2018-06-25 18:10:17 +0800

    修改距离展示方法

--- GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/util/GPSUtil.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/util/GPSUtil.java
@@ -1,5 +1,8 @@
 package com.tempus.gss.product.hol.api.util;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class GPSUtil {
 	private static final double EARTH_RADIUS = 6378.137;
 	private static double rad(double d)
@@ -15,8 +18,10 @@ public class GPSUtil {
 	    double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) + 
 	     Math.cos(radLat1)*Math.cos(radLat2)*Math.pow(Math.sin(b/2),2)));
 	    s = s * EARTH_RADIUS;
-	    s = Math.round(s * 10000) / 10000;
-	    return s;
+	   // s = Math.round(s * 100000) / 100000;
+	    BigDecimal bg = new BigDecimal(s).setScale(2, RoundingMode.UP);//UP四舍五入
+	    return bg.doubleValue();
+	    
 	}
 	public static void main(String[] args) {
 		double s = getDistance(22.54642109902500000000,113.97567112334000000000,22.53212629322500000000,113.92600195984000000000);
