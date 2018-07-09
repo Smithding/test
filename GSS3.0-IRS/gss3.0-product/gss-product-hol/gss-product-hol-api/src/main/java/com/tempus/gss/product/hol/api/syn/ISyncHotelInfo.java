1fe4fe8069a7c78f65d13cc875850db8dfb3f36c
kai.yang <kai.yang@tempus.cn>
2018-07-09 17:36:29 +0800

    增加酒店列表方法

--- GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/syn/ISyncHotelInfo.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/syn/ISyncHotelInfo.java
@@ -17,6 +17,8 @@ public interface ISyncHotelInfo {
 	
 	public ResBaseInfo newQueryHotelDetail(Agent agent, Long resId, String startTime, String endTime) throws GSSException;
 	
+	public ResBaseInfo newQueryHolProDetail(Agent agent, Long resId, String uniProId, String startTime, String endTime) throws GSSException;
+	
 	public <T> T queryDetailById(Long id, Class<T> clazz) throws GSSException;
 	
 	/**
