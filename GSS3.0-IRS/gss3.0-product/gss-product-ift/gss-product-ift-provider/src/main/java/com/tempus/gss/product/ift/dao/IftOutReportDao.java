e418097553b05e15a23ef33024facd70e6982a51
yiyuan.zhang@tempus.cn <yiyuan.zhang@tempus.cn>
2018-07-18 15:42:45 +0800

    国际机票报表

--- GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-provider/src/main/java/com/tempus/gss/product/ift/dao/IftOutReportDao.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-provider/src/main/java/com/tempus/gss/product/ift/dao/IftOutReportDao.java
@@ -1,15 +1,15 @@
 package com.tempus.gss.product.ift.dao;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import com.tempus.gss.product.ift.api.entity.IftOutReport;
-import com.tempus.gss.product.ift.api.entity.vo.QueryIftOutReportVo;
+import com.tempus.gss.product.ift.api.entity.IftOutReportVO;
 import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
 
 public interface IftOutReportDao {
-
-
-     List<IftOutReport> queryIftOutReport(RowBounds page, QueryIftOutReportVo query);
-
-     List<String> getDept_nameList();
+    
+    List<IftOutReport> queryIftOutReport(RowBounds page, IftOutReportVO query);
+    
+    List<String> getDept_nameList();
 }
