fee151c7e5cab13607247ab55eeecba5eb1e29f9
yangkai <kai.yang@tempus.cn>
2018-08-08 21:29:35 +0800

    修改同程请求类

--- GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/syn/ISyncHotelInfo.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-api/src/main/java/com/tempus/gss/product/hol/api/syn/ISyncHotelInfo.java
@@ -15,7 +15,7 @@ public interface ISyncHotelInfo {
 	
 	public Future<ResBaseInfo> newQueryHotelDetail(Agent agent, Long resId, String startTime, String endTime) throws GSSException;
 	
-	public Future<ResBaseInfo> queryProDetail(Agent agent, Long resId, String startTime, String endTime) throws GSSException;
+	public ResBaseInfo queryProDetail(Agent agent, Long resId, String startTime, String endTime) throws GSSException;
 	
 	public ResBaseInfo newQueryHolProDetail(Agent agent, Long resId, String uniProId, String startTime, String endTime) throws GSSException;
 	
