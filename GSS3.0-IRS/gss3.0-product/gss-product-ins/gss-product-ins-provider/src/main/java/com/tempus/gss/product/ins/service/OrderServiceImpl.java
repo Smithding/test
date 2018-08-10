6760d43fd14f49f28c9d2f1900bbf0f57d5b368d
root <liang.ling@tempus.cn>
2018-08-10 14:40:39 +0800

    保存保险渠道名

--- GSS3.0-IRS/gss3.0-product/gss-product-ins/gss-product-ins-provider/src/main/java/com/tempus/gss/product/ins/service/OrderServiceImpl.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-ins/gss-product-ins-provider/src/main/java/com/tempus/gss/product/ins/service/OrderServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import com.tempus.gss.system.service.GSSException;
 import org.apache.commons.lang3.StringUtils;
 import org.jfree.util.Log;
 import org.slf4j.Logger;
