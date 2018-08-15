9890891bb86fe672c6fbae160e0fea05d9a67b34
yangkai <kai.yang@tempus.cn>
2018-08-15 18:34:26 +0800

    修改打印日志

--- GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-provider/src/main/java/com/tempus/gss/product/hol/utils/LockMethodInterceptor.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-hol/gss-product-hol-provider/src/main/java/com/tempus/gss/product/hol/utils/LockMethodInterceptor.java
@@ -3,6 +3,8 @@ package com.tempus.gss.product.hol.utils;
 import java.lang.reflect.Method;
 import java.util.UUID;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,16 +14,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 
-
 @Aspect
 @Component
 public class LockMethodInterceptor {
+	protected final transient Logger logger = LogManager.getLogger(LockMethodInterceptor.class);
 	
 	@Autowired
     RedisLockHol redisLockHol;
 	
     @Around("execution(* com.tempus.gss.product.hol.service..*(..)) && @annotation(com.tempus.gss.product.hol.utils.CacheHolLock)")
-    public Object interceptor(ProceedingJoinPoint pjp) {
+    public Object interceptor(ProceedingJoinPoint pjp) throws RuntimeException{
         MethodSignature signature = (MethodSignature) pjp.getSignature();
         Method method = signature.getMethod();
         CacheHolLock lock = method.getAnnotation(CacheHolLock.class);
@@ -39,16 +41,18 @@ public class LockMethodInterceptor {
             // 假设上锁成功，但是设置过期时间失效，以后拿到的都是 false
             final boolean success = redisLockHol.lock(lockKey, value, lock.expire(), lock.timeUnit());
             if (!success) {
+            	logger.info("推送酒店订单状态变更为重复提交, 重复提交参数为: "+lockKey);
                 throw new RuntimeException("重复提交");
             }
             try {
                 return pjp.proceed();
             } catch (Throwable throwable) {
+            	logger.info("推送酒店订单状态变更为重复提交, 重复提交参数为: "+lockKey);
                 throw new RuntimeException(throwable.getMessage());
             }
         }
         finally {
-        	redisLockHol.unlock(lockKey, value);
+        	//redisLockHol.unlock(lockKey, value);
         }
     }
     
