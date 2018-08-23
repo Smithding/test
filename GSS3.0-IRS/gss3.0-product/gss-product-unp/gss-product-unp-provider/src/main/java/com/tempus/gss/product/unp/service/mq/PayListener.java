5a40e83fc9561a0a3050415439eff83cd2a06a0a
yiyuan.zhang@tempus.cn <yiyuan.zhang@tempus.cn>
2018-08-23 16:05:16 +0800

    去除之前的废代码

--- GSS3.0-IRS/gss3.0-product/gss-product-unp/gss-product-unp-provider/src/main/java/com/tempus/gss/product/unp/service/mq/PayListener.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-unp/gss-product-unp-provider/src/main/java/com/tempus/gss/product/unp/service/mq/PayListener.java
@@ -1,13 +1,11 @@
-package com.tempus.gss.product.unp.mq;
+package com.tempus.gss.product.unp.service.mq;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSON;
 import com.tempus.gss.order.entity.enums.GoodsBigType;
 import com.tempus.gss.order.entity.vo.PayNoticeVO;
 import com.tempus.gss.order.entity.vo.PayReceiveVO;
-import com.tempus.gss.product.common.entity.RequestWithActor;
-import com.tempus.gss.product.unp.api.entity.vo.UnpOrderVo;
-import com.tempus.gss.product.unp.api.service.IUnpOrderService;
+import com.tempus.gss.product.unp.api.service.UnpOrderService;
 import com.tempus.gss.vo.Agent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +26,7 @@ public class PayListener {
     
     private Logger logger = LoggerFactory.getLogger(PayListener.class);
     @Reference
-    IUnpOrderService unpOrderService;
+    UnpOrderService unpOrderService;
     
     @RabbitHandler
     public void processLogRecord(PayNoticeVO payNoticeVO) {
@@ -37,24 +35,6 @@ public class PayListener {
             // 商品类型 1 国内机票 2 国际机票 3 保险 4 酒店 5 机场服务 6 配送 9通用产品
             if (GoodsBigType.GENERAL.getKey() == payNoticeVO.getGoodsType() && PayReceiveVO.PS_PAY_STATUS_SUCCESS == payNoticeVO.getPayStatus()) {
                 Agent agent = payNoticeVO.getAgent();
-                if (payNoticeVO.getBusinessType() == 2 && payNoticeVO.getChangeType() == 0) {
-                    int payType = payNoticeVO.getPayWay() / 1000000;
-                    UnpOrderVo unpOrderVo = new UnpOrderVo();
-                    unpOrderVo.setOrderNo(payNoticeVO.getBusinessNo());
-                    unpOrderVo.setSaleAccountType(payType);
-                    //已支付
-                    RequestWithActor<UnpOrderVo> request = new RequestWithActor<>();
-                    request.setAgent(agent);
-                    request.setEntity(unpOrderVo);
-                    try {
-                        
-                        boolean update = unpOrderService.updateOrderPayStatus(request);
-                        logger.info("修改通用产品支付状态--{}", update);
-                    } catch (Exception e) {
-                        logger.error("修改通用产品支付状态 Error", e);
-                    }
-                    
-                }
             }
         } catch (Exception ex) {
             logger.error("消费队列异常 ：", ex);
