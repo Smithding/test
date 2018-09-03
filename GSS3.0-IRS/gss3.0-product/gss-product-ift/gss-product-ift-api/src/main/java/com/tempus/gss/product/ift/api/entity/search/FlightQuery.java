4f67407aca120f1fde813f9f7de10063ec3b81d5
root <zhao.chen@tempus.cn>
2018-09-03 09:10:27 +0800

    新建国际查询参数

---
+++ GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-api/src/main/java/com/tempus/gss/product/ift/api/entity/search/FlightQuery.java
@@ -0,0 +1,26 @@
+package com.tempus.gss.product.ift.api.entity.search;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * <pre>
+ * <b>国际航班查询参数实体类.</b>
+ * <b>Description:</b> 
+ *    
+ * <b>Author:</b> cz
+ * <b>Date:</b> 2018年9月3日 上午9:08:03
+ * <b>Copyright:</b> Copyright ©2018 tempus.cn. All rights reserved.
+ * <b>Changelog:</b>
+ *   Ver   Date                    Author                Detail
+ *   ----------------------------------------------------------------------
+ *   0.1   2018年9月3日 上午9:08:03    cz
+ *         new file.
+ * </pre>
+ */
+public class FlightQuery implements Serializable{
+
+	/** SVUID */
+	private static final long serialVersionUID = 1L;
+	
+}
