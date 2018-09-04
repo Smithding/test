ee629c4abfe6e5ea1d8f212fd671f7192965d351
yinjuan <juan.juan@tempus.cn>
2018-09-04 09:29:02 +0800

    国际政策

--- GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-api/src/main/java/com/tempus/gss/product/ift/api/entity/search/FlightQuery.java
+++ GSS3.0-IRS/gss3.0-product/gss-product-ift/gss-product-ift-api/src/main/java/com/tempus/gss/product/ift/api/entity/search/FlightQuery.java
@@ -19,27 +19,27 @@ import java.util.Date;
  *         new file.
  * </pre>
  */
-public class FlightQuery implements Serializable{
+public class FlightQuery implements Serializable {
 
 	/** SVUID */
 	private static final long serialVersionUID = 1L;
-	
+
 	/** 归集单位 */
 	private int owner;
 	/** 实际出票航空公司 */
 	private String airline;
 	/** 航程类型, 1:单程(默认); 2:往返; */
-	private String voyageType;
-	/**去程航班起飞时间 2018-09-03*/
+	private Integer voyageType;
+	/** 去程航班起飞时间 2018-09-03 */
 	private Date flyDate;
-	/**回程起飞时间 2018-09-09*/
+	/** 回程起飞时间 2018-09-09 */
 	private Date rtnFlyDate;
-	/**当前销售时间 08:30，*/
-	private String saleHous;
-	/**当前星期几 1，2，3，4，5，6，7*/
+	/** 当前星期几 1，2，3，4，5，6，7 */
 	private int saleWeek;
 	/** 出发所属洲 */
 	private String departContinent;
+	/** 出发城市国际I，还是国内D */
+	private String departSign;
 	/** 出发国家 */
 	private String departCountry;
 	/** 出发机场，多个以"/"分割 */
@@ -48,6 +48,8 @@ public class FlightQuery implements Serializable{
 	private String[] departCabin;
 	/** 中转机场，多个以"/"分割 */
 	private String[] transitAirport;
+	/** 中转机场国际I，还是国内D */
+	private String transitSign;
 	/** 中转舱位 */
 	private String[] transitCabin;
 	/** 到达所属洲 */
@@ -56,117 +58,199 @@ public class FlightQuery implements Serializable{
 	private String arriveCountry;
 	/** 到达机场，多个以"/"分割 */
 	private String arriveAirport;
-	/**乘客人数*/
+	/** 到达机场国际I，还是国内D */
+	private String arriveSign;
+	/** 乘客人数 */
 	private int psgerNum;
-	/**乘客类型 1：成人；2:儿童;3:婴儿；4：成人+儿童；5成人+婴儿；*/
+	/** 乘机双数true;单数false */
+	private Boolean psgerEven;
+	/** 乘客类型 1：成人；2:儿童;3:婴儿；4：成人+儿童；5成人+婴儿； */
 	private int psgerType;
-	
-	
+	/** 去程星期 1，2，3，4，5，6，7 */
+	private int departWeek;
+	/** 回程星期 */
+	private int rtnDepartWeek;
+
+	public String getDepartSign() {
+		return departSign;
+	}
+
+	public void setDepartSign(String departSign) {
+		this.departSign = departSign;
+	}
+
+	public String getTransitSign() {
+		return transitSign;
+	}
+
+	public void setTransitSign(String transitSign) {
+		this.transitSign = transitSign;
+	}
+
+	public String getArriveSign() {
+		return arriveSign;
+	}
+
+	public void setArriveSign(String arriveSign) {
+		this.arriveSign = arriveSign;
+	}
+
+	public Boolean getPsgerEven() {
+		return psgerEven;
+	}
+
+	public void setPsgerEven(Boolean psgerEven) {
+		this.psgerEven = psgerEven;
+	}
+
+	public int getDepartWeek() {
+		return departWeek;
+	}
+
+	public void setDepartWeek(int departWeek) {
+		this.departWeek = departWeek;
+	}
+
+	public int getRtnDepartWeek() {
+		return rtnDepartWeek;
+	}
+
+	public void setRtnDepartWeek(int rtnDepartWeek) {
+		this.rtnDepartWeek = rtnDepartWeek;
+	}
+
 	public int getOwner() {
 		return owner;
 	}
+
 	public void setOwner(int owner) {
 		this.owner = owner;
 	}
+
 	public String getAirline() {
 		return airline;
 	}
+
 	public void setAirline(String airline) {
 		this.airline = airline;
 	}
-	public String getVoyageType() {
+
+	public Integer getVoyageType() {
 		return voyageType;
 	}
-	public void setVoyageType(String voyageType) {
+
+	public void setVoyageType(Integer voyageType) {
 		this.voyageType = voyageType;
 	}
+
 	public Date getFlyDate() {
 		return flyDate;
 	}
+
 	public void setFlyDate(Date flyDate) {
 		this.flyDate = flyDate;
 	}
+
 	public Date getRtnFlyDate() {
 		return rtnFlyDate;
 	}
+
 	public void setRtnFlyDate(Date rtnFlyDate) {
 		this.rtnFlyDate = rtnFlyDate;
 	}
-	public String getSaleHous() {
-		return saleHous;
-	}
-	public void setSaleHous(String saleHous) {
-		this.saleHous = saleHous;
-	}
+
 	public int getSaleWeek() {
 		return saleWeek;
 	}
+
 	public void setSaleWeek(int saleWeek) {
 		this.saleWeek = saleWeek;
 	}
+
 	public String getDepartContinent() {
 		return departContinent;
 	}
+
 	public void setDepartContinent(String departContinent) {
 		this.departContinent = departContinent;
 	}
+
 	public String getDepartCountry() {
 		return departCountry;
 	}
+
 	public void setDepartCountry(String departCountry) {
 		this.departCountry = departCountry;
 	}
+
 	public String getDepartAirport() {
 		return departAirport;
 	}
+
 	public void setDepartAirport(String departAirport) {
 		this.departAirport = departAirport;
 	}
+
 	public String[] getDepartCabin() {
 		return departCabin;
 	}
+
 	public void setDepartCabin(String[] departCabin) {
 		this.departCabin = departCabin;
 	}
+
 	public String[] getTransitAirport() {
 		return transitAirport;
 	}
+
 	public void setTransitAirport(String[] transitAirport) {
 		this.transitAirport = transitAirport;
 	}
+
 	public String[] getTransitCabin() {
 		return transitCabin;
 	}
+
 	public void setTransitCabin(String[] transitCabin) {
 		this.transitCabin = transitCabin;
 	}
+
 	public String getArriveContinent() {
 		return arriveContinent;
 	}
+
 	public void setArriveContinent(String arriveContinent) {
 		this.arriveContinent = arriveContinent;
 	}
+
 	public String getArriveCountry() {
 		return arriveCountry;
 	}
+
 	public void setArriveCountry(String arriveCountry) {
 		this.arriveCountry = arriveCountry;
 	}
+
 	public String getArriveAirport() {
 		return arriveAirport;
 	}
+
 	public void setArriveAirport(String arriveAirport) {
 		this.arriveAirport = arriveAirport;
 	}
+
 	public int getPsgerNum() {
 		return psgerNum;
 	}
+
 	public void setPsgerNum(int psgerNum) {
 		this.psgerNum = psgerNum;
 	}
+
 	public int getPsgerType() {
 		return psgerType;
 	}
+
 	public void setPsgerType(int psgerType) {
 		this.psgerType = psgerType;
 	}
